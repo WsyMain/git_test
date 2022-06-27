999d8c25f673dce8993ca6908f2b59ac1756bf70
wsyGit <wsyGit@wsy.com>
2022-06-27 20:56:22 +0800

    4 commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("Hello Git22!");
         System.out.println("Hello Git33!");
+        System.out.println("王世玉");
     }
 }
